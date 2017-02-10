9bf799837bc2dcdefdf0aa0b5e5ed64f46c31986
Sebastian Laag <sebastian.laag@googlemail.com>
2017-02-10 23:18:06 +0100

    created build scripts and a start class

---
+++ web/src/main/java/de/laag/CaketasticApplication.java
@@ -0,0 +1,11 @@
+package de.laag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaketasticApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CaketasticApplication.class);
+    }
+}
